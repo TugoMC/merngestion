84b6d9337f0b923adf13b7e49a6162da337a5897
Junior Ouattara <83041354+TugoMC@users.noreply.github.com>
2025-04-19 20:12:22 +0000

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000';
+const API_URL = 'https://merngestion-backend.onrender.com';
 
 const api = axios.create({
     baseURL: API_URL,
